e2007e5d0a941e74f630999304940cef4b166a95
AR31313 <gurlgotgame@gmail.com>
2022-04-20 09:50:03 -0700

    questions added

--- index.js
+++ index.js
@@ -1,35 +1,86 @@
-
 const inquirer = require('inquirer');
 const fs = require("fs");
 
 inquirer
   .prompt([
     {
-      type: 'input', //type of font
-      message: 'What is your name?',
-      name: 'username', //property on the result object
+      type: "input",
+      name: "projectTitle",
+      message: "What is the project title?",
     },
     {
-      type: 'input',
-      message: 'What is your Location?',
-      name: 'location', //property on the result object
+      type: "input",
+      name: "description",
+      message: "Write a brief description of your project: "
     },
     {
-      type: 'input',
-      message: 'Tell me about yourself.',
-      name: 'bio', //property on the result object
+      type: "input",
+      name: "sitelink",
+      message: "Please post the link the Deployed Application ",
     },
     {
-      type: 'list', //shows list of options
-      message: 'Which acct would you like to share?',
-      choices: ['Linkedin', 'Github', 'Facebook'],
-      name: 'socialmedia',
+      type: "input",
+      name: "behavior",
+      message: "What is this Expected Behavior of the Application"
     },
     {
-      type: 'input', //type of font
-      message: 'Paste your URL',
-      name: 'URL', //property on the result object
+      type: "list",
+      name: "install",
+      message: "Is thes an installation process? ",
+      choices: [
+        "Yes",
+        "No",
+      ]
     },
+    {
+      type: "input",
+      name: "installprocess",
+      message: "What is the  for the Application?"
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Chose the appropriate license for this project: ",
+      choices: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open"
+      ]
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who are the contributors of this projects?"
+    },
+    {
+      type: "input",
+      name: "tablecontent",
+      message: "what are the Table of Contents?"
+    },
+    {
+      type: "input",
+      name: "bugs",
+      message: "Are there any known bugs?"
+    },
+    {
+      type: "input",
+      name: "repolink",
+      message: "Please post the Application's GitHub repo link ?"
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Please enter your GitHub username: "
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email: "
+    }
   ])
 
   .then((data) => {
